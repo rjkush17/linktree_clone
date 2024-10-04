778a8c624f88cd14b8e78876652ffceedf5be218
rjkush17 <rishabhkushwahrj@gmail.com>
2024-10-04 06:53:27 +0530

    navbar created

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "home#index"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
